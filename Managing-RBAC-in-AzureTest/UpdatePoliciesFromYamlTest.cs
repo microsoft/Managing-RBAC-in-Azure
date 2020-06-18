c2cb504fe37010b7fc14d3f750f6a5ffee687e17
Katie Helman <t-kahelm@microsoft.com>
2020-06-18 16:49:08 -0400

    Added unit tests

--- Managing-RBAC-in-AzureTest/UpdatePoliciesFromYamlTest.cs
+++ Managing-RBAC-in-AzureTest/UpdatePoliciesFromYamlTest.cs
@@ -28,8 +28,92 @@ namespace RBAC
             Assert.IsTrue(expectedYamlVaults.SequenceEqual(yamlVaults));
         }
 
+        [TestMethod]
+        public void TestCheckChanges()
+        {
+            UpdatePoliciesFromYaml up = new UpdatePoliciesFromYaml(true);
+            List<KeyVaultProperties> vaultsRetrieved = createExpectedYamlVaults();
+            List<KeyVaultProperties> yamlVaults = vaultsRetrieved;
+
+            // Check making 6 changes (first two only count as one change) 
+            yamlVaults[0].AccessPolicies[0].PermissionsToSecrets = new string[] { "get" };
+            yamlVaults[0].AccessPolicies[0].PermissionsToCertificates = new string[] { "get" };
+            yamlVaults[0].AccessPolicies[1].PermissionsToCertificates = new string[] { "get" };
+            yamlVaults[0].AccessPolicies[2].PermissionsToCertificates = new string[] { "get" };
+            yamlVaults[0].AccessPolicies[3].PermissionsToCertificates = new string[] { "get" };
+            yamlVaults[1].AccessPolicies[0].PermissionsToKeys = new string[] { "get" };
+            yamlVaults[1].AccessPolicies[1].PermissionsToCertificates = new string[] { "get" };
+
+            try
+            {
+                up.checkChanges(yamlVaults, vaultsRetrieved);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Error: You have changed too many policies. The maximum is {Constants.MAX_NUM_CHANGES}, but you have changed 6 policies.", e.Message);
+                //exit
+            }
+
+            // Add an entire KV
+            yamlVaults.Add(new KeyVaultProperties
+            {
+                VaultName = "TestAddKV",
+                ResourceGroupName = "RBAC",
+                SubscriptionId = "subid",
+                Location = "eastus",
+                TenantId = "tenant",
+                AccessPolicies = new List<PrincipalPermissions>()
+                {
+                    new PrincipalPermissions
+                    {
+                        Type = "Service Principal",
+                        DisplayName = "RBACAutomationApp",
+                        Alias = "",
+                        PermissionsToKeys = new string[] { "get", "list", "update", "create", "import", "delete", "recover", "backup", "restore" },
+                        PermissionsToSecrets = new string[] {  "get", "list", "set", "delete", "recover", "backup", "restore" },
+                        PermissionsToCertificates = new string[] { "get", "list", "update", "create", "import", "delete", "recover", "backup", "restore", "managecontacts", "manageissuers", "getissuers", "listissuers", "setissuers", "deleteissuers" }
+                    },
+                    new PrincipalPermissions
+                    {
+                        Type = "User",
+                        DisplayName = "Katie Helman",
+                        Alias = "t-kahelm@microsoft.com",
+                        PermissionsToKeys = new string[] {  "get", "list", "update", "create", "import", "delete", "recover", "backup", "restore" },
+                        PermissionsToSecrets = new string[] { "get", "list", "set", "delete", "recover", "backup", "restore" },
+                        PermissionsToCertificates = new string[] { }
+                    }
+                }
+            });
+
+            try
+            {
+                up.checkChanges(yamlVaults, vaultsRetrieved);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Error: KeyVault, TestAddKV, in the YAML file was not found in the JSON file.", e.Message);
+                //exit
+            }
+            yamlVaults.RemoveAt(4);
+
+            // Remove an entire KV
+            yamlVaults.RemoveAt(0);
+            try
+            {
+                up.checkChanges(yamlVaults, vaultsRetrieved);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Error: KeyVault, RG1Test1, specified in the JSON file was not found in the YAML file.", e.Message);
+                //exit
+            }
+        }
+
        [TestMethod]
-       public void CheckSPFields()
+       public void TestCheckSPFields()
         {
             UpdatePoliciesFromYaml up = new UpdatePoliciesFromYaml(true);
             PrincipalPermissions sp = new PrincipalPermissions()
@@ -171,17 +255,102 @@ namespace RBAC
                     }
                 }
             };
+            AccessPoliciesToYaml ap = new AccessPoliciesToYaml(true);
+            JsonInput json = ap.readJsonFile("../../../input/MasterConfig.json");
+            var secrets = ap.getSecrets(json);
 
             try
             {
-                AccessPoliciesToYaml ap = new AccessPoliciesToYaml(true);
-                JsonInput json = ap.readJsonFile("../../../input/MasterConfig.json");
-                var secrets = ap.getSecrets(json);
                 up.updateVaults(new List<KeyVaultProperties>() { kv }, new List<KeyVaultProperties> { }, ap.createKVMClient(secrets), secrets, ap.createGraphClient(secrets));
             }
             catch (Exception e)
             {
                 Assert.AreEqual($"Error: RG1Test2 does not contain at least two users. Vault Skipped.", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestCheckValidPermissions()
+        {
+            UpdatePoliciesFromYaml up = new UpdatePoliciesFromYaml(true);
+            PrincipalPermissions sp = new PrincipalPermissions
+            {
+                Type = "Group",
+                DisplayName = "TestGroup",
+                Alias = "1essre@microsoft.com",
+                PermissionsToKeys = new string[] { "getS", "list" },
+                PermissionsToSecrets = new string[] { },
+                PermissionsToCertificates = new string[] { }
+            };
+
+            try
+            {
+                up.checkValidPermissions(sp);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Invalid key permission 'getS'", e.Message); //thre it thro so wehre caught?
+            }
+
+            sp.PermissionsToKeys = new string[] { };
+            sp.PermissionsToSecrets = new string[] { "managecontacts", "list" };
+            try
+            {
+                up.checkValidPermissions(sp);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Invalid secret permission 'managecontacts'", e.Message); 
+            }
+
+            sp.PermissionsToSecrets = new string[] { };
+            sp.PermissionsToCertificates = new string[] { "managecontactz", "managecontactz" };
+            try
+            {
+                up.checkValidPermissions(sp);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Invalid certificate permission 'managecontactz'", e.Message);
+            }
+
+            sp.PermissionsToCertificates = new string[] { };
+            sp.PermissionsToKeys = new string[] { "read", "read", "purge", "purge"};
+            try
+            {
+                up.checkValidPermissions(sp);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Key permission(s) 'read, purge' repeated", e.Message); 
+            }
+
+            sp.PermissionsToKeys = new string[] { };
+            sp.PermissionsToSecrets = new string[] { "get", "      get" };
+            try
+            {
+                up.checkValidPermissions(sp);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Secret permission(s) 'get' repeated", e.Message);
+            }
+
+            sp.PermissionsToSecrets = new string[] { };
+            sp.PermissionsToCertificates = new string[] { "all - PURGE", "all - purge" };
+            try
+            {
+                up.checkValidPermissions(sp);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Secret permission(s) 'get' repeated", e.Message);
             }
         }
 
