afdb6bdab212e5b44c7d3120bd92727116358d58
Katie Helman <t-kahelm@microsoft.com>
2020-06-24 15:34:21 -0400

    used APIs

--- Code/Constants.cs
+++ Code/Constants.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using Microsoft.Azure.Management.KeyVault.Models;
+using System.Linq;
+using System;
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
+
 namespace RBAC
 {
     /// <summary>
@@ -20,24 +23,23 @@ namespace RBAC
         public const int MIN_NUM_USERS = 2;
         public const int MAX_NUM_CHANGES = 5;
 
+        //Defines the user types
         public enum USER_TYPES { User, Group, ServicePrincipal, Application }
         // Defines shorthands for keys
-        public enum ALL_KEY_PERMISSIONS { get, list, update, create, import, delete, recover,
-            backup, restore, decrypt, encrypt, unwrapkey, wrapkey, verify, sign, purge }
-        public enum READ_KEY_PERMISSIONS { get, list };
+        public static readonly string[] ALL_KEY_PERMISSIONS = typeof(KeyPermissions).GetFields().Select(prop => prop.Name.ToLower()).ToArray();
+        public enum READ_KEY_PERMISSIONS { get, list }
         public enum WRITE_KEY_PERMISSIONS { update, create, delete }
         public enum STORAGE_KEY_PERMISSIONS { import, recover, backup, restore }
         public enum CRYPTOGRAPHIC_KEY_PERMISSIONS { decrypt, encrypt, unwrapkey, wrapkey, verify, sign }
 
         // Defines shorthands for secrets
-        public enum ALL_SECRET_PERMISSIONS { get, list, set, delete, recover, backup, restore, purge }
+        public static readonly string[] ALL_SECRET_PERMISSIONS = typeof(SecretPermissions).GetFields().Select(prop => prop.Name.ToLower()).ToArray();
         public enum READ_SECRET_PERMISSIONS { get, list }
         public enum WRITE_SECRET_PERMISSIONS { set, delete }
         public enum STORAGE_SECRET_PERMISSIONS { recover, backup, restore }
 
         // Defines shorthands for certificates
-        public enum ALL_CERTIFICATE_PERMISSIONS { get, list, update, create, import, delete, recover, backup, restore, managecontacts,
-            manageissuers, getissuers, listissuers, setissuers, deleteissuers, purge }
+        public static readonly string[] ALL_CERTIFICATE_PERMISSIONS = typeof(CertificatePermissions).GetFields().Select(prop => prop.Name.ToLower()).ToArray();
         public enum READ_CERTIFICATE_PERMISSIONS { get, list }
         public enum WRITE_CERTIFICATE_PERMISSIONS { update, create, delete }
         public enum STORAGE_CERTIFICATE_PERMISSIONS { import, recover, backup, restore }
@@ -49,17 +51,8 @@ namespace RBAC
         public enum SHORTHANDS_CERTIFICATES { all, read, write, storage, management }
 
         // Defines all valid permissions
-        public enum VALID_KEY_PERMISSIONS
-        {
-            get, list, update, create, import, delete, recover, backup, restore, decrypt, 
-            encrypt, unwrapkey, wrapkey, verify, sign, purge, all, read, write, storage, crypto
-        }
-        public enum VALID_SECRET_PERMISSIONS { get, list, set, delete, recover, backup, restore, purge, all, read, write, storage }
-        public enum VALID_CERTIFICATE_PERMISSIONS
-        {
-            get, list, update, create, import, delete, recover, backup, restore, managecontacts,
-            manageissuers, getissuers, listissuers, setissuers, deleteissuers, purge,
-            all, read, write, storage, management
-        }
+        public static readonly string[] VALID_KEY_PERMISSIONS = ALL_KEY_PERMISSIONS.Concat(Enum.GetNames(typeof(SHORTHANDS_KEYS))).ToArray();
+        public static readonly string[] VALID_SECRET_PERMISSIONS = ALL_SECRET_PERMISSIONS.Concat(Enum.GetNames(typeof(SHORTHANDS_SECRETS))).ToArray();
+        public static readonly string[] VALID_CERTIFICATE_PERMISSIONS = ALL_CERTIFICATE_PERMISSIONS.Concat(Enum.GetNames(typeof(SHORTHANDS_CERTIFICATES))).ToArray();
     }
 }
