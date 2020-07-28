5aad65e216845e7ef3e79314b517fb1859855599
Katie Helman <t-kahelm@microsoft.com>
2020-07-28 09:34:32 -0400

    Method comments

--- ListingOptions/FileDialogWindow.xaml.cs
+++ ListingOptions/FileDialogWindow.xaml.cs
@@ -23,6 +23,11 @@ namespace RBAC
             InitializeComponent();
         }
 
+        /// <summary>
+        /// This method opens a file dialog and uses the file specified to initialize the yaml instance in the main window.
+        /// </summary>
+        /// <param name="sender">The "Select File" button</param>
+        /// <param name="e">The event that occurs when a button is clicked</param>
         private void FileDialogRun_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
@@ -41,12 +46,22 @@ namespace RBAC
             }
         }
 
+        /// <summary>
+        /// This method makes the button a different color when the user hovers over it.
+        /// </summary>
+        /// <param name="sender">The "Select File" button</param>
+        /// <param name="e">The event that occurs when a mouse hovers over the button</param>
         private void Button_MouseEnter(object sender, MouseEventArgs e)
         {
             Button btn = sender as Button;
             btn.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb(14, 77, 101));
         }
 
+        /// <summary>
+        /// This method returns the button to its original color when a user is no longer hovering over the button.
+        /// </summary>
+        /// <param name="sender">The "Select File" button</param>
+        /// <param name="e">The event that occurs when a mouse exits the button area</param>
         private void Button_MouseLeave(object sender, MouseEventArgs e)
         {
             Button btn = sender as Button;
